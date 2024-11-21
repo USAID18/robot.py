2f722e1e86833a18699b9dded3d2d9e0c49ed7fa
USAID198 <imranjamilke@gmail.com>
2024-11-21 06:53:43 -0800

    meesage

---
+++ robo.py
@@ -0,0 +1,17 @@
+class robo:
+    species = "electronic"
+
+
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+
+
+robo = parrot("susani",immortal)
+
+
+
+
+print(robo.name,"is",robo.age,"years old")
+
